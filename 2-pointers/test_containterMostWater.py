32099312ed7fb4db13887bc7ab0f9ae6ef42ddf5
vikkastaneja <vikkastaneja@users.noreply.github.com>
2025-01-16 00:14:32 -0800

    Increased code coverage

--- 2-pointers/test_containterMostWater.py
+++ 2-pointers/test_containterMostWater.py
@@ -5,7 +5,8 @@ testdata = [([1,8,6,2,5,4,8,3,7], 49),
             ([1,1], 1),
             ([4,3,2,1,4], 16),
             ([1,2,1], 2),
-            ([3,4,1,2,2,4,1,3,2], 21)]
+            ([3,4,1,2,2,4,1,3,2], 21),
+            ([] , 0)]
 
 @pytest.mark.parametrize("heights, expected", testdata)
 def test_max_area(heights, expected):
